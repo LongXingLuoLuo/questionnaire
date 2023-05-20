bfa08371df70424010fa4dd71bd473201a67f78f
LongXingLuoLuo <2473475362@qq.com>
2023-05-21 03:03:50 +0800

    添加了数据库初始化，增加实体，删除实体等测试函数

--- src/test/java/com/longxingluoluo/questionnaire/QuestionnaireApplicationTests.java
+++ src/test/java/com/longxingluoluo/questionnaire/QuestionnaireApplicationTests.java
@@ -1,12 +1,16 @@
 package com.longxingluoluo.questionnaire;
 
-import com.longxingluoluo.questionnaire.entity.Curriculum;
-import com.longxingluoluo.questionnaire.service.CurriculumEvaluationService;
-import com.longxingluoluo.questionnaire.service.CurriculumService;
+import com.alibaba.fastjson.JSONObject;
+import com.longxingluoluo.questionnaire.dao.CurriculumEvaluationDao;
+import com.longxingluoluo.questionnaire.dao.QuestionnaireAnswerDao;
+import com.longxingluoluo.questionnaire.entity.*;
+import com.longxingluoluo.questionnaire.service.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.*;
 
 @SpringBootTest
 class QuestionnaireApplicationTests {
@@ -18,18 +22,81 @@ class QuestionnaireApplicationTests {
     @Resource
     CurriculumService curriculumService;
     @Resource
-    CurriculumEvaluationService curriculumEvaluationService;
+    TeacherService teacherService;
+    @Resource
+    GradeService gradeService;
+    @Resource
+    ProfessionalService professionalService;
+    @Resource
+    QuestionnaireService questionnaireService;
+    @Resource
+    QuestionnaireAnswerService questionnaireAnswerService;
+
+
     @Test
-    void CurriculumTest(){
-        System.out.println(curriculumService.addNewByName("curriculum1"));
-        System.out.println(curriculumService.findAll());
+    void CurriculumTest() {
+        curriculumService.deleteById(4L);
+    }
+
+    /**
+     * 测试当 Curriculum 删除时, QuestionnaireAnswer, CurriculumEvaluation 是否删除
+     */
+    @Test
+    void questionnaireAnswerDeleteTest() {
+        questionnaireAnswerService.deleteById(1L);
     }
 
     @Test
-    void CurriculumEvaluationTest(){
+    void jsonFindAllTest(){
+//        List<QuestionnaireAnswer> questionnaireAnswerList = questionnaireAnswerDao.findAll();
+        System.out.println(JSONObject.toJSONString(questionnaireAnswerService.findAll()));
+    }
+
+    @Test
+    void initTestDatabase(){
+        List<Curriculum> curriculumList = new ArrayList<>();
+        curriculumList.add(curriculumService.addNewByName("大学物理"));
+        curriculumList.add(curriculumService.addNewByName("电子技术实习"));
+        List<Teacher> teacherList = new ArrayList<>();
+        teacherList.add(teacherService.addNewByName("张三"));
+        teacherList.add(teacherService.addNewByName("李四"));
+        List<Grade> gradeList = new ArrayList<>();
+        gradeList.add(gradeService.addNewByName("本科2022"));
+        gradeList.add(gradeService.addNewByName("本科2021"));
+        gradeList.add(gradeService.addNewByName("本科2020"));
+        gradeList.add(gradeService.addNewByName("本科2019"));
+        gradeList.add(gradeService.addNewByName("本科2018"));
+        gradeList.add(gradeService.addNewByName("本科2017"));
+        List<Professional> professionalList = new ArrayList<>();
+        professionalList.add(professionalService.addNewByName("计算机科学技术"));
+        professionalList.add(professionalService.addNewByName("物联网"));
+        professionalList.add(professionalService.addNewByName("信息安全"));
+        questionnaireService.addNewByAll("调查问卷-1",gradeList, professionalList, curriculumList, teacherList);
+    }
+
+    @Test
+    void questionnaireAnswerAddTest(){
+        List<CurriculumEvaluation> curriculumEvaluationList = new ArrayList<>();
+        CurriculumEvaluation curriculumEvaluation = new CurriculumEvaluation();
         Curriculum curriculum = new Curriculum();
         curriculum.setId(2L);
-        curriculum.setName("curriculum2");
-        curriculumEvaluationService.addNewByCurriculumAndEvaluation(curriculum, 5);
+        curriculumEvaluation.setCurriculum(curriculum);
+        curriculumEvaluation.setEvaluation(15);
+        curriculumEvaluationList.add(curriculumEvaluation);
+        List<TeacherEvaluation> teacherEvaluationList = new ArrayList<>();
+        TeacherEvaluation teacherEvaluation = new TeacherEvaluation();
+        Teacher teacher = new Teacher();
+        teacher.setId(1L);
+        teacherEvaluation.setTeacher(teacher);
+        teacherEvaluation.setEvaluation(20);
+        teacherEvaluationList.add(teacherEvaluation);
+        Grade grade = new Grade();
+        grade.setId(1L);
+        Professional professional = new Professional();
+        professional.setId(1L);
+        Questionnaire questionnaire = new Questionnaire();
+        questionnaire.setId(1L);
+        int selfEvaluation = 10;
+        questionnaireAnswerService.addNewByAll(questionnaire, grade, professional, curriculumEvaluationList, teacherEvaluationList, selfEvaluation);
     }
 }
