78221f4a36a156c3f4ac6d7265dd82d661255199
LongXingLuoLuo <2473475362@qq.com>
2023-05-23 00:36:22 +0800

    TAG 设置为 final

--- src/test/java/com/longxingluoluo/questionnaire/QuestionnaireApplicationTests.java
+++ src/test/java/com/longxingluoluo/questionnaire/QuestionnaireApplicationTests.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Log4j2
 @SpringBootTest
 class QuestionnaireApplicationTests {
-    private String TAG = "TEST: ";
+    private final String TAG = "TEST: ";
 
     @Test
     void contextLoads() {
